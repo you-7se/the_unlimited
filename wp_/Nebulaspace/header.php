ab228202073825cdcd38ccf2325f9fe36de78ada
you-7se <you_nanase@mirror.ocn.ne.jp>
2020-05-14 18:56:04 +0900

    WordPress用のファイル一式をコミットしました。(v01)

---
+++ wp_/Nebulaspace/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>ブログ | Sirius X</title>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/ress.css" type="text/css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" type="text/css">
+        <!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> -->
+        <?php wp_enqueue_script('jquery'); ?>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <div class="bg">
+            <div class="header-contents">
+                <div class="header-area">
+                    <h1 class="blog-title">Sirius X</h1>
+                    <p class ="blog-expl">需要無視の自己満足ブログです。</p>
+                </div>
+            </div>
---
+++ wp_/Nebulaspace/index.php
@@ -0,0 +1,111 @@
+<?php get_header(); ?>
+
+            <div id="MainBlock">
+                <div class="entry-area">
+                    <div class="singles">
+                        <h2 class="entry-title">エントリータイトル“１”</h2>
+                        <h2 class="entry-data">Category：雑記　Comment：0</h2>
+                        <h3 class="entry-datetimes">2020-01-01 12:30:00 posted.</h3>
+                        <div class="entry">
+                            <p>Hello,World!!</p>
+                            <p>日本語の表示<br>改行します。Carriage Return & Line Feed</p>
+                            <p class="fw-bold">太字です。</p>
+                            <p class="fc-red">赤字です。#ff0000。</p>
+                            <p class="fs-up1">文字サイズを一段階上げます。</p>
+                            <p class="fs-up2">文字サイズを二段階上げます。</p>
+                            <p class="fs-up3">文字サイズを三段階上げます。</p>
+                            <p class="fw-bold fc-red fs-up3">赤字 + 太字 + 文字サイズ拡大です。</p>
+                            <ul>
+                                <li>ブログの体裁を整える</li>
+                                <li>ヘッダとフッタのスタイルを考える</li>
+                                <li>全体のデザインを調整する<br>　（見栄えを優先したレイアウト変更）</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="singles">
+                        <h2 class="entry-title">エントリータイトル“２”</h2>
+                        <h2 class="entry-data">Category：雑記　Comment：0</h2>
+                        <h3 class="entry-datetimes">2020-01-01 12:30:00 posted.</h3>
+                        <div class="entry">
+                            <p>Hello,World!!</p>
+                            <p>日本語の表示<br>改行します。Carriage Return & Line Feed</p>
+                            <p class="fw-bold">太字です。</p>
+                            <p class="fc-red">赤字です。#ff0000。</p>
+                            <p class="fs-up1">文字サイズを一段階上げます。</p>
+                            <p class="fs-up2">文字サイズを二段階上げます。</p>
+                            <p class="fs-up3">文字サイズを三段階上げます。</p>
+                            <p class="fw-bold fc-red fs-up3">赤字 + 太字 + 文字サイズ拡大です。</p>
+                            <ul>
+                                <li>ブログの体裁を整える</li>
+                                <li>ヘッダとフッタのスタイルを考える</li>
+                                <li>全体のデザインを調整する<br>　（見栄えを優先したレイアウト変更）</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="singles">
+                        <h2 class="entry-title">エントリータイトル“３”</h2>
+                        <h2 class="entry-data">Category：雑記　Comment：0</h2>
+                        <h3 class="entry-datetimes">2020-01-01 12:30:00 posted.</h3>
+                        <div class="entry">
+                            <p>Hello,World!!</p>
+                            <p>日本語の表示<br>改行します。Carriage Return & Line Feed</p>
+                            <p class="fw-bold">太字です。</p>
+                            <p class="fc-red">赤字です。#ff0000。</p>
+                            <p class="fs-up1">文字サイズを一段階上げます。</p>
+                            <p class="fs-up2">文字サイズを二段階上げます。</p>
+                            <p class="fs-up3">文字サイズを三段階上げます。</p>
+                            <p class="fw-bold fc-red fs-up3">赤字 + 太字 + 文字サイズ拡大です。</p>
+                            <ul>
+                                <li>ブログの体裁を整える</li>
+                                <li>ヘッダとフッタのスタイルを考える</li>
+                                <li>全体のデザインを調整する<br>　（見栄えを優先したレイアウト変更）</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="singles">
+                        <h2 class="entry-title">エントリータイトル“４”</h2>
+                        <h2 class="entry-data">Category：雑記　Comment：0</h2>
+                        <h3 class="entry-datetimes">2020-01-01 12:30:00 posted.</h3>
+                        <div class="entry">
+                            <p>Hello,World!!</p>
+                            <p>日本語の表示<br>改行します。Carriage Return & Line Feed</p>
+                            <p class="fw-bold">太字です。</p>
+                            <p class="fc-red">赤字です。#ff0000。</p>
+                            <p class="fs-up1">文字サイズを一段階上げます。</p>
+                            <p class="fs-up2">文字サイズを二段階上げます。</p>
+                            <p class="fs-up3">文字サイズを三段階上げます。</p>
+                            <p class="fw-bold fc-red fs-up3">赤字 + 太字 + 文字サイズ拡大です。</p>
+                            <ul>
+                                <li>ブログの体裁を整える</li>
+                                <li>ヘッダとフッタのスタイルを考える</li>
+                                <li>全体のデザインを調整する<br>　（見栄えを優先したレイアウト変更）</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="singles">
+                        <h2 class="entry-title">エントリータイトル“５”</h2>
+                        <h2 class="entry-data">Category：雑記　Comment：0</h2>
+                        <h3 class="entry-datetimes">2020-01-01 12:30:00 posted.</h3>
+                        <div class="entry">
+                            <p>Hello,World!!</p>
+                            <p>日本語の表示<br>改行します。Carriage Return & Line Feed</p>
+                            <p class="fw-bold">太字です。</p>
+                            <p class="fc-red">赤字です。#ff0000。</p>
+                            <p class="fs-up1">文字サイズを一段階上げます。</p>
+                            <p class="fs-up2">文字サイズを二段階上げます。</p>
+                            <p class="fs-up3">文字サイズを三段階上げます。</p>
+                            <p class="fw-bold fc-red fs-up3">赤字 + 太字 + 文字サイズ拡大です。</p>
+                            <ul>
+                                <li>ブログの体裁を整える</li>
+                                <li>ヘッダとフッタのスタイルを考える</li>
+                                <li>全体のデザインを調整する<br>　（見栄えを優先したレイアウト変更）</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-area">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+
+<?php get_footer(); ?>
