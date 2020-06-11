8bbdb264066d643d1d5b3e5dac49acbfae155161
you-7se <you_nanase@mirror.ocn.ne.jp>
2020-06-11 16:08:01 +0900

    2020-06-11 修正。 ・archive.phpに更新日時ロジックが漏れていたので追加。 ・style.cssの内容を一部修正してzipファイル作り直し。 ・ウェブサイトトップページを再作成。

--- wp_/Nebulaspace/archive.php
+++ wp_/Nebulaspace/archive.php
@@ -5,7 +5,7 @@
             <div id="mainContainer" class="entry-area">
                 <?php
                     // 投稿の取得を試す
-                    if ( have_posts() ) : 
+                    if ( have_posts() ) :
                         while ( have_posts() ) :
                             // 記事の取得
                             the_post();
@@ -13,7 +13,10 @@
                             // 記事カテゴリの取得
                             $post_id = get_the_ID();
                             $html_category = get_categories_for_entry( $post_id );
-                            
+
+                            // 更新日時の取得
+                            $updated_date = the_last_updated_for_entry( $post_id );
+
                             // 投稿日付が現時点より一定期間内であれば、任意のタグを追加する (最新記事に「NEW！」アイコンを付与する)
                             $post_date = new datetime(get_the_time( 'Y-m-d H:i:s' ));
                             $now_date = new datetime( date("Y-m-d H:i:s", strtotime("-5 day")) );
@@ -28,7 +31,7 @@
                     <h2 class="entry-title"><?php echo $bland_new_mark; ?><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <h2 class="entry-data">Category：<?php echo $html_category; ?>　Comment：<a href="<?php comments_link(); ?>"><?php echo get_comments_number(); ?></a></h2>
                     <h3 class="entry-datetimes"><?php the_time('Y-m-d H:i:s'); ?> posted.</h3>
-                    <h3 class="entry-datetimes"><?php the_modified_date('Y-m-d H:i:s'); ?> last updated.</h3>
+                    <h3 class="entry-datetimes"><?php echo $updated_date; ?></h3>
                     <div class="entry">
                         <!-- ここから記事本文 -->
                         <?php the_excerpt(); ?>
@@ -41,13 +44,13 @@
                         if ( function_exists( 'pagination' ) ) :
                             pagination( $wp_query->max_num_pages, get_query_var( 'paged' ) );
                         endif;
-                    
+
                     else :
                         echo 'まだ記事がありません。';
                     endif;
                 ?>
             </div>
-            
+
             <!-- 右カラム・メニューバー -->
             <div id="subContainer" class="item-area">
                 <?php get_sidebar(); ?>
--- wp_/Nebulaspace/header.php
+++ wp_/Nebulaspace/header.php
@@ -4,12 +4,12 @@
         <meta charset="UTF-8">
         <title>ブログ | Sirius X</title>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/ress.css" type="text/css">
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" type="text/css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/custom-design.css" type="text/css">
         <!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> -->
         <?php wp_enqueue_script('jquery'); ?>
         <?php wp_head(); ?>
-        
-        <?php 
+
+        <?php
             // 【JavaScriptを使用】 ナビゲーションメニューバーを固定
             if ( is_admin_bar_showing() ) {
                 $add_class_value = "nav.addClass('fixed-admin'); head.addClass('fixed');";
@@ -45,18 +45,17 @@
         <script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js");</script>
         <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
         <!-- ここまで -->
-        
+
         <div id="naviMenu" class="navigation-menu-bar">
             <!-- ナビゲーションメニューを追加する -->
             <?php wp_nav_menu(); ?>
         </div>
-        
+
         <div id="headerArea" class="header-contents">
             <h1 class="blog-title"><?php bloginfo('name'); ?></h1>
             <p class ="blog-expl"><?php bloginfo('description'); ?></p>
         </div>
-        
+
         <div class="breads">
             <?php breadcrumbs(); ?>
         </div>
-            
